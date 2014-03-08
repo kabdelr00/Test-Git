b5cbcacca951d2c090fe2d9f2f50dad08c0c606a
xx <xx@xx-s-mbp.westell.com>
2014-03-08 18:04:08 -0500

    Initial Commit

---
+++ Git Test/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Git Test
+//
+//  Created by xx  on 3/8/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
